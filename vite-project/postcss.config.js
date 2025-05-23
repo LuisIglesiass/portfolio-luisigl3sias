c4dcc355bf8e96ac192936d5bc256ed771a660b0
Luis Iglesias <luis.iglesias@kuehne-nagel.com>
2025-05-23 15:30:49 +0200

    first commit

---
+++ vite-project/postcss.config.js
@@ -0,0 +1,10 @@
+   // vite-project/postcss.config.js
+   import tailwindcss from '@tailwindcss/postcss';
+   import autoprefixer from 'autoprefixer';
+
+   export default {
+     plugins: [
+       tailwindcss,
+       autoprefixer,
+     ],
+   }
---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+			{/* Theme Toggle */}
+			<div className="bg-red-500 text-white p-4">
+				¡Hola, Jose Antonio!
+			</div>
+			<ThemeToggle />
+			{/* Background Effects */}
+
+			{/* Navbar */}
+
+			{/* Main Content */}
+
+			{/* Footer */}
+        </div>
+    )
+}
