296e7a6de84efd5d8186c9e6acb224818497c16c
Luis Iglesias <luis.iglesias@kuehne-nagel.com>
2025-05-23 16:06:56 +0200

    correct tailwind configuration

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -4,9 +4,6 @@ export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 			{/* Theme Toggle */}
-			<div className="bg-red-500 text-white p-4">
-				¡Hola, Jose Antonio!
-			</div>
 			<ThemeToggle />
 			{/* Background Effects */}
 
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import path from "path"; 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
